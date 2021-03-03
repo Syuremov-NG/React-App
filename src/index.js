80cc325ab40e2558316cbbb743ffcc4c476216e7
Syuremov-NG <neket0331@gmail.com>
2021-03-04 00:56:32 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React, {Component} from "react";
+import ReactDOM from "react-dom";
+import App from "./components/app/app";
+import AppHeader from "./components/app-header/app-header";
+import 'bootstrap/dist/css/bootstrap.css';
+
+ReactDOM.render(<App />, document.getElementById("root"));
