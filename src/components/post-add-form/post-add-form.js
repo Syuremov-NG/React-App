de45cf7c1f7e7c46eaddbdc073769e514ad3b0fe
Syuremov-NG <neket0331@gmail.com>
2021-03-04 12:13:01 +0300

    serverless page ready

--- src/components/post-add-form/post-add-form.js
+++ src/components/post-add-form/post-add-form.js
@@ -18,7 +18,9 @@ export default class PostAddForm extends Component {
 
     onSubmit(e){
         e.preventDefault();
-        this.props.onAdd(this.state.text); 
+        if(this.state.text != ''){
+           this.props.onAdd(this.state.text);  
+        }
         this.setState({
             text: ''
         });
